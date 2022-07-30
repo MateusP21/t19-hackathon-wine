1abf381735ee542f5992618b4ba008269fa66d3a
MateusP21 <mateuspasso@outlook.com>
2022-07-30 13:04:09 -0300

    feat: header adicionado

--- landing-page/src/components/Header/index.js
+++ landing-page/src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { HeaderContainer, HeaderNav } from './styles';
 
 const Header = () => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <HeaderContainer>
+    <h1>Wine</h1>
+    <HeaderNav>
+      <ul>
+        <li>Home</li>
+        <li>Clube</li>
+        <li>Loja</li>
+        <li>Produtores</li>
+        <li>Ofertas</li>
+        <li>Eventos</li>
+      </ul>
+    </HeaderNav>
+  </HeaderContainer>
 );
 
 export default Header;
--- landing-page/src/components/Header/styles.js
+++ landing-page/src/components/Header/styles.js
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+  display:flex;
+  padding: 1rem;
+  background-color: violet;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const HeaderLogo = styled.img``;
+export const HeaderNav = styled.nav`
+  display:flex;
+  ul {
+    display: flex;
+    background-color: yellow;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    gap: 1rem;
+    li {
+      list-style-type: none;
+    }
+  }
+`;
